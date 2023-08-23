ad3647d84d405b8e9974697bde52f0b189fb8945
mariana-barreto27 <mariana.cunha@polo.ufsc.br>
2023-08-23 18:32:47 -0300

    commit changes feitas no lab

--- src/dummy_src_file.jl
+++ src/dummy_src_file.jl
@@ -3,26 +3,32 @@ B=[ 3; 5; 7.5; 8] #Matriz B
 # Método de eliminação de Gauss
 function fgauss(A::Array, B::Array)
     matriz_expandida= [ A B ]
+    print(matriz_expandida)
     tamanho_matriz= size(matriz_expandida,1)
-    println(tamanho_matriz)
-    for i in 1 : tamanho_matriz-1
-        dividendo= matriz_expandida[i,i]
-        for j in i+1 : tamanho_matriz
-                aux= matriz_expandida[j,i]/dividendo
-                matriz_expandida[j,:]=matriz_expandida[j,:]- (aux .*matriz_expandida[i,:])
-            
-            
+    for k in 1 : tamanho_matriz-1
+        for i in k+1 : tamanho_matriz
+                aux= matriz_expandida[i,k]/matriz_expandida[k,k]
+                matriz_expandida[i,k]=0
+                for j in k+1 : tamanho_matriz+1
+                    matriz_expandida[i,j]=matriz_expandida[i,j] - (aux * matriz_expandida[k,j])
+                end            
         end
     end
     println(matriz_expandida) 
-    X=
-    X[1,tamanho_matriz] = matriz_expandida[tamanho_matriz,tamanho_matriz+1]/ matriz_expandida[tamanho_matriz,tamanho_matriz]
-    for i in tamanho_matriz-1:1:-1
-        somatorio=0
-        for j in i+1 : tamanho_matriz
-            somatorio= somatorio + matriz_expandida[i,j] * X[j,1]
-        end
-    X[i,1]=(matriz_expandida[i, tamanho_matriz+1]-somatorio)/ matriz_expandida[i,i]
+    #X=rand[0, tamanho_matriz , 1]
+    #println(X)
+    #X=[0,0,0,0]
+    for i in 1: tamanho_matriz
+        aux2= (matriz_expandida[i,end]/matriz_expandida[i,i])
+        println(aux2)
     end
-println(X)
+    #for i in tamanho_matriz-1:1:-1
+     #   somatorio=0
+      #  for j in i+1 : tamanho_matriz
+       #     somatorio= somatorio + matriz_expandida[i,j] * X[j,1]
+        #    println(somatorio)
+        #end
+    #X[i,1]=(matriz_expandida[i, tamanho_matriz+1]-somatorio)/ matriz_expandida[i,i]
+    #end
+#println(X)
 end
