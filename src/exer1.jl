9c954994e79021b272bb80d1fcee90620f8cf4e9
mariana-barreto27 <mariana.cunha@polo.ufsc.br>
2023-08-29 13:52:17 -0300

    coloquei o ex 1 na pasta errada

--- src/exer1.jl
+++
@@ -1,8 +0,0 @@
-
-A = [2 1 3 5 ; 1 2 7 8 ; 3 4 9 5; (π) 2*(π) (ℯ) (ℯ)^(π) ] #matriz A 
-B=[ 3; 5; 7.5; 8] #Matriz B
-# Método de eliminação de Gauss
-C= [ A B ] #Criação de uma matriz extendida 
-
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
