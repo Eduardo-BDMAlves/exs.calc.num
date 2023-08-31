869e0eb142648ccee5e5f2f6a17904087f6bebcf
mariana-barreto27 <mariana.cunha@polo.ufsc.br>
2023-08-31 17:41:44 -0300

    arrumando tent1

--- src/GaussSeidel.jl
+++ src/GaussSeidel.jl
@@ -1,13 +1,13 @@
 using LinearAlgebra
 function gaussseidel(A,B, ϵ, max_inter)
     n=size(A)
-    X=zeros(n,1)
     while ϵ> 1e-6
+        X=zeros(n,1)
         for k in 1:max_inter
             matriz_atualiz=X
             for i in 1:n
-                soma=A[i,(i+1:n)]*X[(i+1):n]
-                X[i,1]= (A[i,n+1]- soma)/A[i,i]
+                soma=sum(A(i,(i+1:n)) * X((i+1):n))
+                X[i,1]= (A(i,n+1)- soma)/A(i,i)
             end
             ϵ=max(abs(X-matriz_atualiz))
             k=k+1
