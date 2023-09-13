3961f6e315d8a517354971dddec64be0b618af35
mariana-barreto27 <mariana.cunha@polo.ufsc.br>
2023-09-13 08:00:26 -0300

    documentation, tentativa monte carlo pi, otimizado e tentativa falha de criar o MonteCarloPlot; não roda  a parte do include

--- scripts/MonteCarloPlot.jl
+++ scripts/MonteCarloPlot.jl
@@ -1,41 +1,11 @@
-# using Pkg
-# Pkg.add("Plots")
-using Random
-
-function monte_carlo_pi(n)
-    inside = 0
-    x_inside = [] # especificar tipo ex: Float64[]
-    y_inside = []
-    x_outside = []
-    y_outside = []
-
-    for _ in 1:n
-        x, y = rand(), rand()
-        # if sqrt(x^2 + y^2) <= 1.0 # precisa dessa raiz?
-        if x^2 + y^2 <= 1.0
-            inside += 1
-            push!(x_inside, x)
-            push!(y_inside, y)
-        else
-            push!(x_outside, x)
-            push!(y_outside, y)
-        end
-    end
-
-    pi_estimate = 4 * inside / n
-    return pi_estimate, x_inside, y_inside, x_outside, y_outside
-end
-function monte_carlo_pi(n,x_inside,y_inside,x_outside,y_outside)
-    
-end
-
+using DrWatson
+@quickactivate ("MonteCarloPlot.jl")
+include(srcdir("monte_carlo_pi.jl"))
 n = 100_000  # Número de pontos gerados
 # pi_estimate, x_inside, y_inside, x_outside, y_outside = monte_carlo_pi(n) # defnindo os pontos pela função
 
 # println("Estimativa de π: $pi_estimate") #estimativa: nao consegui colocar o while para fazer com a precisão certa
-
-
-n_iterations = 100_000
+n_iterations = 100
 error = zeros(n_iterations)
 for i in 1:n_iterations
     pi_estimate, x_inside, y_inside, x_outside, y_outside = monte_carlo_pi(n) # defnindo os pontos pela função
@@ -47,7 +17,7 @@ for i in 1:n_iterations
 end
 
 
-pi_estimate, x_inside, y_inside, x_outside, y_outside = monte_carlo_pi(n*1000) # defnindo os pontos pela função
+pi_estimate, x_inside, y_inside, x_outside, y_outside = monte_carlo_pi(n) # defnindo os pontos pela função
 
 using Plots
 
